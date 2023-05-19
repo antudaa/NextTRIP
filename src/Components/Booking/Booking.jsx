4df54ced25392657316bf730cb57b363cd867798
Shamimayesmin <shamimayesmin144@gmail.com>
2023-05-19 23:33:09 +0600

    new check in input

--- src/Components/Booking/Booking.jsx
+++ src/Components/Booking/Booking.jsx
@@ -153,7 +153,7 @@ const Booking = () => {
 										</div>
 									</div>
 									<div className="grid lg:grid-cols-1 md:grid-cols-1 gap-3">
-										<div>
+										{/* <div>
 											<label className="font-medium">Travellers</label>
 											<input
 												{...register("travell", {
@@ -164,13 +164,13 @@ const Booking = () => {
 												placeholder=" Total travellers !"
 												className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
 											/>
-											{/* Error Message  */}
+											Error Message 
 											{errors.travell && (
 												<p className="text-red-500" role="alert">
 													{errors.travell?.message}
 												</p>
 											)}
-										</div>
+										</div> */}
 										
 									</div>
 									{/* Submit Button  */}
