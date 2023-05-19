a2b60a0d562f218e62fb237eb0205bf97e43e388
Shamimayesmin <shamimayesmin144@gmail.com>
2023-05-19 23:35:07 +0600

    check out input

--- src/Components/Booking/Booking.jsx
+++ src/Components/Booking/Booking.jsx
@@ -130,27 +130,27 @@ const Booking = () => {
 												</p>
 											)}
 										</div>
-										<div>
+										{/* <div>
 											<label className="font-medium">Check-out</label>
 											
 												<input
 													type="date"
                                                     name="backtime"
 													placeholder="Add return date !"
-													// Password Validation
+													
 													{...register("checkout", {
 														required: "Put check out date",
 													})}
 													className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
 												/>
 											
-											{/* Error Message  */}
+											Error Message 
 											{errors.checkout && (
 												<p className="text-red-500" role="alert">
 													{errors.checkout?.message}
 												</p>
 											)}
-										</div>
+										</div> */}
 									</div>
 									<div className="grid lg:grid-cols-1 md:grid-cols-1 gap-3">
 										<div>
