2baaec6a82af4858f1d741ad010c444736336e87
Shamimayesmin <shamimayesmin144@gmail.com>
2023-05-19 23:35:35 +0600

    validation

--- src/Components/Booking/Booking.jsx
+++ src/Components/Booking/Booking.jsx
@@ -130,7 +130,7 @@ const Booking = () => {
 												</p>
 											)}
 										</div>
-										{/* <div>
+										<div>
 											<label className="font-medium">Check-out</label>
 											
 												<input
@@ -144,13 +144,13 @@ const Booking = () => {
 													className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
 												/>
 											
-											Error Message 
+											{/* Error Message  */}
 											{errors.checkout && (
 												<p className="text-red-500" role="alert">
 													{errors.checkout?.message}
 												</p>
 											)}
-										</div> */}
+										</div>
 									</div>
 									<div className="grid lg:grid-cols-1 md:grid-cols-1 gap-3">
 										<div>
