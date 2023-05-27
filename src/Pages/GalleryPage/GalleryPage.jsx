5137ae9d1bda5b190d3a155345041aa22b75e32e
Shamimayesmin <shamimayesmin144@gmail.com>
2023-05-27 21:40:42 +0600

    new slider

--- src/Pages/GalleryPage/GalleryPage.jsx
+++ src/Pages/GalleryPage/GalleryPage.jsx
@@ -1,12 +1,27 @@
 import React from "react";
 
-import img1 from '../../assets/images/beach.jpg'
-import img2 from '../../assets/images/bot.jpg'
-import img3 from '../../assets/images/city6.jpg'
+// swiper slider
+// import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+
+// import "./styles.css";
+
+// import required modules
+import { EffectCoverflow, Pagination } from "swiper";
+
+import img1 from "../../assets/images/beach.jpg";
+import img2 from "../../assets/images/bot.jpg";
+import img3 from "../../assets/images/city6.jpg";
 const GalleryPage = () => {
 	return (
 		<div className="container mx-auto px-5 py-2 lg:px-32 lg:pt-24">
-			<div className="-m-1 flex flex-wrap md:-m-2">
+			{/* <div className="-m-1 flex flex-wrap md:-m-2">
 				<div className="flex w-1/2 flex-wrap">
 					<div className="w-1/2 p-1 md:p-2">
 						<img
@@ -53,7 +68,57 @@ const GalleryPage = () => {
 						/>
 					</div>
 				</div>
-			</div>
+			</div> */}
+
+			<>
+				<Swiper
+					effect={"coverflow"}
+					grabCursor={true}
+					centeredSlides={true}
+					slidesPerView={"auto"}
+					coverflowEffect={{
+						rotate: 50,
+						stretch: 0,
+						depth: 100,
+						width: 50,
+						modifier: 1,
+						slideShadows: true,
+					}}
+					pagination={true}
+					modules={[EffectCoverflow, Pagination]}
+					className="mySwiper"
+				>
+					<div>
+						<SwiperSlide className="w-[40%]">
+							<img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+						</SwiperSlide>
+						<SwiperSlide className="w-[40%]">
+							<img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+						</SwiperSlide>
+						<SwiperSlide className="w-[40%]">
+							<img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+						</SwiperSlide>
+						<SwiperSlide className="w-[40%]">
+							<img src="https://swiperjs.com/demos/images/nature-4.jpg" />
+						</SwiperSlide>
+						<SwiperSlide className="w-[40%]">
+							<img src="https://swiperjs.com/demos/images/nature-5.jpg" />
+						</SwiperSlide>
+						<SwiperSlide className="w-[40%]">
+							<img src="https://swiperjs.com/demos/images/nature-6.jpg" />
+						</SwiperSlide>
+						<SwiperSlide className="w-[40%]">
+							<img src="https://swiperjs.com/demos/images/nature-7.jpg" />
+						</SwiperSlide>
+						<SwiperSlide className="w-[40%]">
+							<img src="https://swiperjs.com/demos/images/nature-8.jpg" />
+						</SwiperSlide>
+						<SwiperSlide className="w-[40%]">
+							<img src="https://swiperjs.com/demos/images/nature-9.jpg" />
+						</SwiperSlide>
+					</div>
+				</Swiper>
+			</>
 
 			{/* section 2 */}
 
