24b7b7e98b677c567b35b458463ccfa796e5c5ae
Shamimayesmin <shamimayesmin144@gmail.com>
2024-02-29 21:41:48 +0600

    added new branch

--- src/Firebase/Firebase.config.jsx
+++ src/Firebase/Firebase.config.jsx
@@ -21,23 +21,9 @@ export default auth;
 
 
 
-// Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
 
 
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAAnJgdsqbwa9rlg-g_Elus-lQ2NXlYzxE",
-//   authDomain: "next-trip-4681f.firebaseapp.com",
-//   projectId: "next-trip-4681f",
-//   storageBucket: "next-trip-4681f.appspot.com",
-//   messagingSenderId: "912815994552",
-//   appId: "1:912815994552:web:975da712eb9777bfeea64d"
-// };
 
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// export default app;
 
 
 
